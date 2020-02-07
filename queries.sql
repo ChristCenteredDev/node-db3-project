3b9cc0ced32f19658d5bf03783a97627540c5271
William M <wmooney1984@gmail.com>
2020-02-06 21:41:34 -0500

    Query Updated with shorter syntax

--- queries.sql
+++ queries.sql
@@ -12,11 +12,12 @@ JOIN shipper
 ON shipvia = shipper.id
 WHERE [order].orderdate < "2012-08-09"
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
-SELECT product.ProductName, orderdetail.Quantity
-FROM product
-JOIN orderdetail
-ON product.id = productid
-WHERE orderdetail.orderid = 10251
+SELECT p.ProductName, o.Quantity
+FROM product AS p
+JOIN orderdetail AS o
+ON p.id = o.productid
+WHERE o.orderid = 10251
+ORDER BY p.ProductName
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
 SELECT Id AS [Order ID], CompanyName AS [Customer Name], LastName AS [Ordered By] 
 FROM [Order]
